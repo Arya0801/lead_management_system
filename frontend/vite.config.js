0987088ed1b07509e4360430961503946029f877
Arya <aryakumari1964@gmail.com>
2025-09-18 16:11:52 +0530

    vite config2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,23 +2,19 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default defineConfig(({ mode }) => {
-  // Load env file based on current mode (development / production)
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react(), tailwindcss()],
-    css: {
-      postcss: {
-        config: path.resolve(__dirname, 'postcss.config.cjs'),
-      },
-    },
     server: {
       port: 5173,
       proxy: {
         '/api': {
-          target: env.VITE_API_URL ,
+          target: process.env.VITE_API_URL ,
           changeOrigin: true,
           secure: false,
         },
