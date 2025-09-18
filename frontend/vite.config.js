00169014aa2fe9478bdfd9e716593ab001700441
Arya <aryakumari1964@gmail.com>
2025-09-18 16:00:50 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  css: {
-    postcss: {
-      config: path.resolve(__dirname, 'postcss.config.cjs'),
-    },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false
+export default defineConfig(({ mode }) => {
+  // Load env file based on current mode (development / production)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    css: {
+      postcss: {
+        config: path.resolve(__dirname, 'postcss.config.cjs'),
       },
     },
-  },
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL ,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
